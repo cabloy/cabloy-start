3a0adf48ed5effff1a2c2068886d5a9e8b56cfd4
zhennann <zhen.nann@icloud.com>
2026-06-03 22:21:29 +0800

    Update render.tabs.tsx

--- zova/src/suite/a-home/modules/home-layoutweb/src/component/layoutWeb/render.tabs.tsx
+++ zova/src/suite/a-home/modules/home-layoutweb/src/component/layoutWeb/render.tabs.tsx
@@ -1,10 +1,13 @@
-import { VNode } from 'vue';
-import { VBtn, VList, VMenu, VTab, VTabs } from 'vuetify/components';
+import type { VNode } from 'vue';
+
+import { VList, VMenu, VTab, VTabs } from 'vuetify/components';
 import { BeanRenderBase, ClientOnly } from 'zova';
 import { Render } from 'zova-module-a-bean';
 import { $iconName } from 'zova-module-a-icon';
 import { RouteTab, ZRouterViewTabs } from 'zova-module-a-routertabs';
 import { ZItemLink } from 'zova-module-home-base';
+
+import type { TypeMenuItem } from '../../model/menu.js';
 
 @Render()
 export class RenderTabs extends BeanRenderBase {
@@ -28,17 +31,14 @@ export class RenderTabs extends BeanRenderBase {
     const $$modelTabs = this.$$modelTabs;
     const { tabKey, info } = tab;
     const titleLocale = info?.title || '';
+    const className = tabKey === $$modelTabs.tabKeyCurrent ? 'text-primary' : '';
     if (info.folder) {
       const slots = {
         activator: ({ props }) => {
           return (
-            <VBtn
-              style={{ height: 'calc(var(--v-tabs-height))' }}
-              icon={info.icon}
-              variant="text"
-              {...props}
-              text={titleLocale}
-            ></VBtn>
+            <VTab value={tabKey} class={className} prependIcon={this.getTabIcon(tab)} {...props}>
+              {titleLocale}
+            </VTab>
           );
         },
       };
@@ -47,14 +47,16 @@ export class RenderTabs extends BeanRenderBase {
           <VList>
             {info.children
               ?.filter(item => !item.folder)
-              .map(item => {
+              .map((item, index) => {
+                const { href, target, to } = this._buildLinkProps(item as TypeMenuItem);
                 return (
                   <ZItemLink
-                    key={item.link}
+                    key={item.link ?? `${tabKey}:${index}`}
                     title={item.title!}
                     icon={(item.icon as any) ?? $iconName('::none')}
-                    href={item.link && item.external ? item.link : undefined}
-                    to={item.link && !item.external ? item.link : undefined}
+                    href={href}
+                    to={to}
+                    target={target}
                   ></ZItemLink>
                 );
               })}
@@ -62,32 +64,57 @@ export class RenderTabs extends BeanRenderBase {
         </VMenu>
       );
     }
-    // not external
-    if (!info.external) {
-      const className = tabKey === $$modelTabs.tabKeyCurrent ? 'text-primary' : '';
-      return (
-        <VTab
-          key={tabKey}
-          value={tabKey}
-          class={`${className}`}
-          href={info.link && info.external ? info.link : undefined}
-          to={info.link && !info.external ? info.link : undefined}
-          prependIcon={this.getTabIcon(tab)}
-        >
-          {titleLocale}
-        </VTab>
-      );
-    }
-    // external
+    const { href, to } = this._buildLinkProps(info as TypeMenuItem);
+    const attrs = info.external && info.target ? { target: info.target } : {};
     return (
-      <ZItemLink
+      <VTab
         key={tabKey}
-        title={titleLocale}
-        icon={info.icon as any}
-        href={info.link}
-        target={info.target}
-      ></ZItemLink>
+        value={tabKey}
+        class={className}
+        href={href}
+        to={to}
+        tag={info.external ? 'a' : undefined}
+        {...attrs}
+        prependIcon={this.getTabIcon(tab)}
+      >
+        {titleLocale}
+      </VTab>
     );
+  }
+
+  private _buildLinkProps(item: TypeMenuItem) {
+    if (item.folder) return {};
+    if (item.external) {
+      return {
+        href: item.link,
+        target: item.target,
+        to: undefined,
+      };
+    }
+    let to: any;
+    if (item.link) {
+      if (this.$router.isRouterName(item.link)) {
+        to = { name: item.link };
+        if (item.meta?.params) {
+          to.params = item.meta.params;
+        }
+        if (item.meta?.query) {
+          to.query = item.meta.query;
+        }
+      } else if (item.meta?.query) {
+        to = {
+          path: item.link,
+          query: item.meta.query,
+        };
+      } else {
+        to = item.link;
+      }
+    }
+    return {
+      href: undefined,
+      target: undefined,
+      to,
+    };
   }
 
   public getTabIcon(tab: RouteTab) {
