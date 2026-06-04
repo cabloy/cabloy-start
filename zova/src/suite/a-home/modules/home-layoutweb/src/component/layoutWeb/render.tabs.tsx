4167d4c585f3b0922ec0d7cb772131ecc9d8aa2f
zhennann <zhen.nann@icloud.com>
2026-06-04 16:43:52 +0800

    Update render.tabs.tsx

--- zova/src/suite/a-home/modules/home-layoutweb/src/component/layoutWeb/render.tabs.tsx
+++ zova/src/suite/a-home/modules/home-layoutweb/src/component/layoutWeb/render.tabs.tsx
@@ -168,9 +168,7 @@ export class RenderTabs extends BeanRenderBase {
 
   private _isMenuLeafCurrent(item: TypeMenuLeaf): boolean {
     if (item.external || !item.link) return false;
-    const currentRoute = this.$currentRoute;
-    if (!currentRoute) return false;
-    return currentRoute.fullPath === item.link;
+    return this.$router.checkActiveOfFullPath(item.link);
   }
 
   private _getMenuItemKey(item: TypeMenuItem): string {
