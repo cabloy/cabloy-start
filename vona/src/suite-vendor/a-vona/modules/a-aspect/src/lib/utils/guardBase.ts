ff7270f9bb5c97ff1cf42db246baec6ce0dcb8ba
zhennann <zhen.nann@icloud.com>
2026-08-17 13:54:38 +0800

    feat: guard base

---
+++ vona/src/suite-vendor/a-vona/modules/a-aspect/src/lib/utils/guardBase.ts
@@ -0,0 +1,20 @@
+import { BeanBase, type Next } from 'vona';
+
+import type { IDecoratorGuardOptions, IGuardExecute } from '../../types/guard.ts';
+
+export class GuardBase extends BeanBase implements IGuardExecute {
+  async execute(options: IDecoratorGuardOptions, next: Next): Promise<boolean> {
+    const result = await this.check(options);
+    if (!result) {
+      if (options.rejectWhenDismatched !== false) return this.app.throw(403);
+    } else {
+      if (options.passWhenMatched !== false) return true;
+    }
+    // next
+    return next();
+  }
+
+  async check(_options: IDecoratorGuardOptions): Promise<boolean> {
+    throw new Error('Not Implemented');
+  }
+}
