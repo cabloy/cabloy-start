f1efc04719ae5ab91d22d88b99d83149cfe15934
zhennann <zhen.nann@icloud.com>
2026-08-19 15:22:43 +0800

    feat: update

--- zova/src/suite/a-demo/modules/demo-basic/src/routes.ts
+++ zova/src/suite/a-demo/modules/demo-basic/src/routes.ts
@@ -5,15 +5,14 @@ import { ZPageState } from './.metadata/page/state.js';
 import { ZPageStyle } from './.metadata/page/style.js';
 
 export const routes: IModuleRoute[] = [
-  { path: 'state', component: ZPageState, meta: { requiresAuth: false, ssrProfile: 'public' } },
+  { path: 'state', component: ZPageState, meta: { requiresAuth: false, ssrProfile: 'session' } },
   {
     path: 'component',
     component: ZPageComponent,
     meta: {
       requiresAuth: false,
-      ssrProfile: 'public',
-      ssrProfileOptions: { responseCache: { expires: '5m' } },
+      ssrProfile: 'session',
     },
   },
-  { path: 'style', component: ZPageStyle, meta: { requiresAuth: false, ssrProfile: 'public' } },
+  { path: 'style', component: ZPageStyle, meta: { requiresAuth: false, ssrProfile: 'session' } },
 ];
