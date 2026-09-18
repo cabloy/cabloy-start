90cd4c825f41991511a30811838e9f2856930049
zhennann <zhen.nann@icloud.com>
2026-09-18 17:26:08 +0800

    Update student.ts

--- zova/src/suite/a-training/modules/training-student/src/model/student.ts
+++ zova/src/suite/a-training/modules/training-student/src/model/student.ts
@@ -23,7 +23,6 @@ export class ModelStudent extends BeanModelBase {
       queryFn: async () => {
         return (await this.scope.api.trainingStudent.summary({ params: { id } })) ?? null;
       },
-      meta: { disableErrorEffect: true },
     });
   }
 
